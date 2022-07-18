b2d9ae316b77e39f5ab2cc8b53cbe377eebb0792
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-07-18 15:03:18 -0700

    practicing class creation

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int firstNum = 4;
+        double secondNum = 2.5;
+        double sum = firstNum + secondNum;
+        String str = "hello";
+        String newSum = firstNum + str;
+        System.out.println(newSum);
+    }
+}
