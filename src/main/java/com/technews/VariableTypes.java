7959ee737932bf55b121a0cb37e7a1f6c47f171e
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-08-08 13:41:33 -0700

    added comments to help understanding

--- src/main/java/com/technews/VariableTypes.java
+++ src/main/java/com/technews/VariableTypes.java
@@ -1,6 +1,7 @@
 package com.technews;
 
 public class VariableTypes {
+//    static allows access to the methods outside of individual objects
     public static void main(String[] args) {
         int firstNum = 4;
         double secondNum = 2.5;
